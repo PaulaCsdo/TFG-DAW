5dc674afdf00e17d23a65e5c89b20f59e893182b
Pilar <pilarnaviocolon@gmail.com>
2022-04-18 17:03:07 +0200

    cambios en el controller usuario

--- TFG01_PilarNavio_PaulaCasado/src/main/java/com/proyecto/controller/UsuarioController.java
+++ TFG01_PilarNavio_PaulaCasado/src/main/java/com/proyecto/controller/UsuarioController.java
@@ -1,15 +1,29 @@
 package com.proyecto.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.proyecto.modelo.bean.Receta;
+import com.proyecto.modelo.dao.RecetaInt;
+
 @Controller
-@RequestMapping("/usuario")
+@RequestMapping("/usuario/index")
 public class UsuarioController {
-	@GetMapping("/home")
-	public String home() {
-		return "index";
+	
+	@Autowired
+	private RecetaInt recint;
+	
+	
+	@GetMapping("/verTodas")
+	public String home(Model model) {
+		List<Receta> recetas = recint.verRecetas();
+		model.addAttribute("ListaRecetas", recetas);
+		return "recetas";
 	}
 
 }
