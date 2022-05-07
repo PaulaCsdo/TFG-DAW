cd58297115ea06d8e745f225a7f196959d21df35
Pilar <pilarnaviocolon@gmail.com>
2022-05-07 18:59:53 +0200

    Corregida la query de buscar ingredientes en receta por tipo de dieta

--- TFG01_PilarNavio_PaulaCasado/src/main/java/com/proyecto/modelo/repository/IngredienteRecetaRepo.java
+++ TFG01_PilarNavio_PaulaCasado/src/main/java/com/proyecto/modelo/repository/IngredienteRecetaRepo.java
@@ -22,7 +22,11 @@ public interface IngredienteRecetaRepo extends JpaRepository<IngredienteEnReceta
 	@Query("select ir from IngredienteEnReceta ir where ir.receta.nivelCocina.idNivel=?1")
 	public List<IngredienteEnReceta> buscarXNivel(int idNivel);
 	
-	@Query("select ir from IngredienteEnReceta ir where ir.receta.tiposDietas.idTipoDieta=?1")
+	//@Query("select ir from IngredienteEnReceta ir where ir.receta.tiposDietas.idTipoDieta=?1")
+	@Query (value = "select * from ingrediente_en_receta ir " + 
+			"inner join tipo_dieta_receta tdr on ir.id_receta = tdr.id_receta " +
+			"inner join tipos_dietas td on tdr.id_tipo_dieta = td.id_tipo_dieta " +
+			"where td.id_tipo_dieta = ?1", nativeQuery = true)
 	public List<IngredienteEnReceta> buscarXTipo(int idTipoDieta);
 	
 }
